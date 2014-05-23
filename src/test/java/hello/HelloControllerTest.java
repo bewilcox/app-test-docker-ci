5f78efc40cb990d8eb049259d8d9993154d3ca4e
Benoit Wilcox <wilcox.benoit@gmail.com>
2014-05-23 15:28:41 +0200

    Tests

---
+++ src/test/java/hello/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+@DirtiesContext
+public class HelloControllerTest {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    @Test
+    public void testIndex() throws Exception {
+        ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+                "http://localhost:" + this.port, String.class);
+
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals("Greetings from Spring Boot!", entity.getBody());
+    }
+}
