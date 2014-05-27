b21b388a0999d1e32f2028c91475a3d963e9b4f7
vagrant <vagrant@precise64.(none)>
2014-05-27 09:53:04 +0000

    update deployment script

--- delivery-scripts/delivery-stage/build_image.sh
+++ delivery-scripts/delivery-stage/build_image.sh
@@ -3,14 +3,14 @@
 # $1 : Application version
 
 # Arguments verification
-if [ $# -neq 1 ]; then
-    echo "One argument expected : Application version"
-    exit 65 #BADARGS
+if [ $# -ne 1 ]; then
+    echo "ERROR : One argument expected --> Application version (e.q 1.0.1)"
+    exit 65
 fi
 
 # Retrieve binairies
 wget http://192.168.29.101:8081/nexus/content/repositories/releases/org/springframework/gs-spring-boot/$1/gs-spring-boot-$1.war
-mv --force gs-spring-boot-$1.war ../image gs-spring-boot.war
+mv --force gs-spring-boot-$1.war ../image/gs-spring-boot.war
 
 # Build Docker image
-sudo docker build --tag 192.168.29.100:5000/prjexample/webapp:$1 ../image
+sudo docker build --tag 192.168.29.100:5000/prjexample/webapp:$1 --rm=true --no-cache ../image
