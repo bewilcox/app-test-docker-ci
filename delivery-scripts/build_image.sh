7098c694ac652f7aa53455991c67ecece7312f14
vagrant <vagrant@precise64.(none)>
2014-05-28 15:26:22 +0000

    adapt build image

--- delivery-scripts/build_image.sh
+++ delivery-scripts/build_image.sh
@@ -11,7 +11,7 @@ fi
 # Retrieve binairies
 # War suppose to be under the current rep
 #wget http://192.168.29.101:8081/nexus/content/repositories/releases/org/springframework/gs-spring-boot/$1/gs-spring-boot-$1.war
-mv --force ../delivery-scripts/gs-spring-boot-$1.war .../delivery-scripts/gs-spring-boot.war
+mv --force ../gs-spring-boot-$1.war ../delivery-scripts/gs-spring-boot.war
 
 # Build Docker image
 sudo docker build --tag 192.168.29.100:5000/prjexample/webapp:$1 --rm=true .
