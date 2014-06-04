c6cf59a748f0846b86e8decdd99d53f6d9a4b595
Benoit Wilcox <wilcox.benoit@gmail.com>
2014-06-04 19:29:08 +0200

    Add verify image

--- delivery-scripts/verify_image.sh
+++ delivery-scripts/verify_image.sh
@@ -1,35 +1,50 @@
-#!/usr/bin/env sh
+#!/bin/bash
 # PARAMETERS
 #$1 application version
+
+# Arguments verification
+if [ $# -ne 1 ]; then
+    echo "ERROR : One argument expected --> Application version (e.q 1.0.1)"
+    exit 65
+fi
+
+RESULT=1
 
 # image creation verification
 
 # Start container
-JOB_ID = $(sudo docker run -d 192.168.29.100:5000/prjexample/webapp:$1)
-JOB_IP = $(docker inspect --format='{{.NetworkSettings.IPAddress}}' $JOB_ID)
-JOB_STATUS = $(docker inspect --format='{{.State.Running}}' $JOB_ID)
-JOB_EXIT_CODE = $(docker inspect --format='{{.State.ExitCode}}' $JOB_ID)
+JOB_ID=$(sudo docker run -d 192.168.29.100:5000/prjexample/webapp:$1)
+echo "JOB Started :  JOB_ID = $JOB_ID"
+JOB_IP=$(sudo docker inspect --format='{{.NetworkSettings.IPAddress}}' $JOB_ID)
+echo "JOB IP :  JOB_IP = $JOB_IP"
+JOB_STATUS=$(sudo docker inspect --format='{{.State.Running}}' $JOB_ID)
+JOB_EXIT_CODE=$(sudo docker inspect --format='{{.State.ExitCode}}' $JOB_ID)
 
-if [$JOB_EXIT_CODE = 0] && [$JOB_STATUS = "true"]; then
-	while ! curl http://$JOB_IP:8080/
-	do
-	  echo "$(date) - still trying"
-	  sleep 1
-	done
-	echo "$(date) - connected successfully"
+if [ "$JOB_EXIT_CODE" == "0" ] && [ "$JOB_STATUS" == "true" ]; then
+        while ! curl http://$JOB_IP:8080/
+        do
+          echo "Trying to connect to host  : http://$JOB_IP:8080/ ..."
+          sleep 1
+        done
+        echo "connected successfully"
 
-	HEALTH = $(curl http://$JOB_IP:8080/health)
-	if [$HEALTH = "OK"]; then
-		echo "$(date) - ERROR health return not equal to OK ( = $HEALTH)"
-	else
-		echo "$(date) - Application started (health return equal to OK)"
-		return -1
-	fi
+        HEALTH=$(curl http://$JOB_IP:8080/health)
+        if [ "$HEALTH" != "ok" ]; then
+                echo "ERROR health return not equal to OK ( = $HEALTH)"
+                RESULT=0
+        else
+                echo "Application started (health return equal to OK)"
+                RESULT=0
+        fi
 else
-	echo "$(date) - Container not running correctly (Status = $JOB_STATUS, exit_code=$JOB_EXIT_CODE)"
-	return -1
+        echo "$(date) - Container not running correctly (Status = $JOB_STATUS, exit_code=$JOB_EXIT_CODE)"
+        RESULT=0
 fi
 
 # Remove container
 sudo docker stop $JOB_ID
 sudo docker rm -f $JOB_ID
+
+if [ "$RESULT" -eq 1 ]; then
+  exit 1
+fi
