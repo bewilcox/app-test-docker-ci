c5479b08a607c9dd9e642696e19b52da89cff2c8
Benoit Wilcox <wilcox.benoit@gmail.com>
2014-06-04 19:44:57 +0200

    Add verify image

--- delivery-scripts/verify_image.sh
+++ delivery-scripts/verify_image.sh
@@ -8,7 +8,7 @@ if [ $# -ne 1 ]; then
     exit 65
 fi
 
-RESULT=1
+RESULT=0
 
 # image creation verification
 
@@ -21,23 +21,22 @@ JOB_STATUS=$(sudo docker inspect --format='{{.State.Running}}' $JOB_ID)
 JOB_EXIT_CODE=$(sudo docker inspect --format='{{.State.ExitCode}}' $JOB_ID)
 
 if [ "$JOB_EXIT_CODE" == "0" ] && [ "$JOB_STATUS" == "true" ]; then
-        while ! curl http://$JOB_IP:8080/
+        while ! curl --silent http://$JOB_IP:8080/
         do
           echo "Trying to connect to host  : http://$JOB_IP:8080/ ..."
           sleep 1
         done
         echo "connected successfully"
 
-        HEALTH=$(curl http://$JOB_IP:8080/health)
+        HEALTH=$(curl --silent --get http://$JOB_IP:8080/health)
         if [ "$HEALTH" != "ok" ]; then
-                echo "ERROR health return not equal to OK ( = $HEALTH)"
-                RESULT=0
+                echo "ERROR -  health return not equal to OK ( = $HEALTH)"
+                RESULT=1
         else
                 echo "Application started (health return equal to OK)"
-                RESULT=0
         fi
 else
-        echo "$(date) - Container not running correctly (Status = $JOB_STATUS, exit_code=$JOB_EXIT_CODE)"
+        echo "ERROR -  Container not running correctly (Status = $JOB_STATUS, exit_code=$JOB_EXIT_CODE)"
         RESULT=0
 fi
 
@@ -45,6 +44,6 @@ fi
 sudo docker stop $JOB_ID
 sudo docker rm -f $JOB_ID
 
-if [ "$RESULT" -eq 1 ]; then
+if [ "$RESULT" -eq 0 ]; then
   exit 1
 fi
